ef573d076d2ec27ba58bc58d8332945a4c3204e3
Ronny <ronny12301@gmail.com>
2024-01-03 01:57:09 -0600

    TokTik

---
+++ Flutter/toktik/lib/shared/data/local_video_posts.dart
@@ -0,0 +1,47 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Año nuevo',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 102523,
+  },
+  {
+    'name': 'Rio lago bosque fluyendo agua en piedras',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 135343,
+  },
+  {
+    'name': 'Making an Axe Leg Kicker',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 2564320,
+    'views': 12003563,
+  },
+  {
+    'name': 'Telefono en blanco',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Donde dice eso?',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'Aston Martin :))',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Ben Schwab cess slide to alley-oop PStar',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 433032,
+  },
+
+];
+
+
